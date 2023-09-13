3f76cf7c1654b436da85f5f1f35a023b85f67195
Moshiur Rahman <115519883+Moshiur19990915@users.noreply.github.com>
2023-09-13 20:57:46 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(function(){
+	/*=== Navbar ===*/ 
+	$(".navbar-toggler").on('click', function() {
+        $(this).toggleClass("active");
+    });
+	var subMenu = $('.navigation .navbar-nav .sub-menu');
+});
